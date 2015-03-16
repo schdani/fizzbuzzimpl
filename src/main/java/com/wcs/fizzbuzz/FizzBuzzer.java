b7b6c7c61e8603e068b3547e1f4c3770fc91c738
thomas <thomas.trinn@gmail.com>
2015-03-16 20:57:55 +0100

    refactor

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -6,13 +6,13 @@ public class FizzBuzzer {
         final String stringNumber = String.valueOf(number);
 
         StringBuilder result = new StringBuilder();
-        if (number % 3 == 0 || stringNumber.contains("3")) {
+        if (isFizzRuleApplicable(number, stringNumber)) {
             result.append("fizz");
         }
-        if (number % 5 == 0 || stringNumber.contains("5")) {
+        if (isBuzzRuleApplicable(number, stringNumber)) {
             result.append("buzz");
         }
-        if (number % 7 == 0 || stringNumber.contains("7")) {
+        if (isWizzRuleApplicable(number, stringNumber)) {
             result.append("wizz");
         }
         if (result.length() == 0) {
@@ -21,4 +21,15 @@ public class FizzBuzzer {
         return result.toString();
     }
 
+    private boolean isFizzRuleApplicable(int number, final String stringNumber) {
+        return number % 3 == 0 || stringNumber.contains("3");
+    }
+
+    private boolean isBuzzRuleApplicable(int number, final String stringNumber) {
+        return number % 5 == 0 || stringNumber.contains("5");
+    }
+    
+    private boolean isWizzRuleApplicable(int number, final String stringNumber) {
+        return number % 7 == 0 || stringNumber.contains("7");
+    }
 }
