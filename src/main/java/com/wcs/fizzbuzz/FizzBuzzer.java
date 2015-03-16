7f735549b4bf47da3e8506a2a0d12c51f805cfa8
thomas <thomas.trinn@gmail.com>
2015-03-16 20:23:20 +0100

    fizzbuzz for numbers wich are multiple of 15

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,13 +3,17 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
     
     public String execute(int number) {
+        StringBuilder result = new StringBuilder();
         if (number % 3 == 0) {
-            return "fizz";
+            result.append("fizz");
         }
         if (number % 5 == 0) {
-            return "buzz";
+            result.append("buzz");
         }
-        return String.valueOf(number);
+        if (result.length() == 0) {
+            return String.valueOf(number);
+        }
+        return result.toString();
     }
     
 }
