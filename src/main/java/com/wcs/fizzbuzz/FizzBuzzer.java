1e79358b20a2e2f55fa3771b3900f4b389de5b23
thomas <thomas.trinn@gmail.com>
2015-03-16 20:20:02 +0100

    buzz for numbers which are multiple of 5

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -6,6 +6,9 @@ public class FizzBuzzer {
         if (number % 3 == 0) {
             return "fizz";
         }
+        if (number % 5 == 0) {
+            return "buzz";
+        }
         return null;
     }
     
